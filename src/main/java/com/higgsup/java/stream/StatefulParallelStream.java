3acd44ce6eb1c0fb4588bf237b7f591b44cde4a0
anhvv <anhvuong.tmdt@gmail.com>
2018-07-17 10:40:41 +0700

    demo stateless lambda expression

---
+++ src/main/java/com/higgsup/java/stream/StatefulParallelStream.java
@@ -0,0 +1,27 @@
+package com.higgsup.java.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StatefulParallelStream {
+
+    public static void main(String[] args) {
+
+        List<Integer> ss = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+        List<Integer> result = new ArrayList<Integer>();
+
+        Stream<Integer> stream = ss.parallelStream();
+
+        stream.map(s -> {
+            synchronized (result) {
+                if (result.size() < 10) {
+                    result.add(s);
+                }
+            }
+            return s;
+        }).forEach( e -> {});
+        System.out.println(result);
+    }
+}
