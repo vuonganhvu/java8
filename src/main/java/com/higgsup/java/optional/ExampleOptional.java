7527f8e27ee7a56610732d7eafa8119307045187
anhvv <anhvuong.tmdt@gmail.com>
2018-07-18 09:49:27 +0700

    demo optional

---
+++ src/main/java/com/higgsup/java/optional/ExampleOptional.java
@@ -0,0 +1,21 @@
+package com.higgsup.java.optional;
+
+import java.util.Optional;
+
+public class ExampleOptional {
+    public static void main(String[] args) {
+        Optional<Student> studentEmpty = Optional.empty();
+        Optional<Student> student = Optional.of(new Student("Anhvv", 10F));
+        System.out.println("--------present value----");
+        student.ifPresent(s->{
+            System.out.println("Student " + s.getName() + " and score is " + s.getScore());
+        });
+        System.out.println("--------empty value----");
+        studentEmpty.ifPresent(s->{
+            System.out.println("Student " + s.getName() + " and score is " + s.getScore());
+        });
+        Student defaultStudent = studentEmpty.orElse(new Student("Higgsup", 5F));
+        System.out.println("--------DefaultStudent----");
+        System.out.println("DefaultStudent is " + defaultStudent);
+    }
+}
